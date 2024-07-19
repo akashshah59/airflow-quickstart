63aca537962abb05c480daf970c465c1d04c251d
Akash Shah <akashshah59@gmail.com>
2024-07-19 21:57:17 +0000

    Making pipeline changes

--- include/global_variables/user_input_variables.py
+++ include/global_variables/user_input_variables.py
@@ -2,7 +2,7 @@
 # Enter your own info! #
 # -------------------- #
 
-MY_NAME = "Friend"
-MY_CITY = "Bern"
-HOT_DAY = 30 # modify what you consider a hot day in degree Celsius
-BIRTH_YEAR = 1990
+MY_NAME = "Akash"
+MY_CITY = "Seattle"
+HOT_DAY = 23 # modify what you consider a hot day in degree Celsius
+BIRTH_YEAR = 1995
